e9fc0e49902dce9ffce70849acf74a678a6018f1
khoon68 <126033125+khoon68@users.noreply.github.com>
2024-07-05 22:58:08 +0900

    초기 세팅

---
+++ src/main/java/aa/account_book/AccountBookApplication.java
@@ -0,0 +1,13 @@
+package aa.account_book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aa/account_book/AccountBookApplicationTests.java
@@ -0,0 +1,13 @@
+package aa.account_book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
